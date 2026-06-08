b757c9ace38f6176b44f4e9401b6e52300897ad3
macdduck <knh9612@naver.com>
2026-06-08 18:29:19 +0900

    의사코드 오타 수정

--- src/codingtestbook/second/stack/Solution12.java
+++ src/codingtestbook/second/stack/Solution12.java
@@ -7,7 +7,7 @@ class Solution12 {
         Deque<Integer> stack = new ArrayDeque<>();
         int[] answer = new int[prices.length];
         // 각 가격들을 stack에 넣으면서 (가격을 넣으면 시간 계산을 할 수 없으므로 index를 넣음)
-        // 떨어졌을 때 시간을 개산하면서 pop();
+        // 떨어졌을 때 시간을 계산하면서 pop();
         for (int i = 0; i < prices.length; i++) {
             while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
                 int index = stack.pop();
